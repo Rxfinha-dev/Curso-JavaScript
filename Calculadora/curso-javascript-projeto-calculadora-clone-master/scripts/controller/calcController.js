120b6c688667a24359ab835c3cdf4d4a394007de
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2024-12-18 08:42:53 -0300

    Switch Para os Operadores

--- Calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
+++ Calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
@@ -1,5 +1,6 @@
 class CalcController {
   constructor() {
+    this._operation = [];
     this._locale = "pt-BR";
     this._displayCalcEl = document.querySelector("#display");
     this._dateEl = document.querySelector("#data");
@@ -16,25 +17,83 @@ class CalcController {
     }, 1000);
   }
 
-  addEventListenerAll(element, events, fn){
-      events.split(' ').forEach(event =>{
-          element.addEventListener(event, fn, false);
-      })
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
+  clearAll() {
+    this._operation = [];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  addOperator(value) {
+    this._operation.push(value);
+
+    console.log(this._operation);
+  }
+
+  setError() {
+    this.displayCalc = "Error";
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case "ac":
+        this.clearAll();
+        break;
+      case "ce":
+        this.clearEntry();
+        break;
+      case "soma":
+        break;
+      case "subtracao":
+        break;
+      case "divisao":
+        break;
+      case "multiplicacao":
+        break;
+      case "porcento":
+        break;
+      case "igual":
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.addOperator(parseInt(value));
+        break;
+
+      default:
+        this.setError();
+        break;
+    }
   }
 
   initButtonsEvents() {
     let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-    buttons.forEach((btn, index) =>{
-        this.addEventListenerAll(btn, "click drag ", (e) => {
-            console.log(btn.className.baseVal.replace("btn-", ""));
-          });
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag ", (e) => {
+        let textBtn = btn.className.baseVal.replace("btn-", "");
 
-          this.addEventListenerAll(btn, "mouseover mousedown mouseup", e=> {
-              btn.style.cursor = "pointer";
-          })
-    })
-    
+        this.execBtn(textBtn);
+      });
+
+      this.addEventListenerAll(btn, "mouseover mousedown mouseup", (e) => {
+        btn.style.cursor = "pointer";
+      });
+    });
   }
 
   setDisplayDateTime() {
@@ -65,7 +124,7 @@ class CalcController {
   }
 
   set displayCalc(valor) {
-    this.displayCalcEl.innerHTML = valor;
+    this._displayCalcEl.innerHTML = valor;
   }
 
   get currentDate() {
