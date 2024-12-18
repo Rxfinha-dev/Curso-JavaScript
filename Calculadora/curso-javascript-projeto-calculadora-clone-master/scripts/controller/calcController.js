3fc3c98530149f682daef74b8584fc02c615a995
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2024-12-18 09:47:30 -0300

    Eval - validação do operador e calculo do resultado

--- Calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
+++ Calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
@@ -35,33 +35,58 @@ class CalcController {
     return this._operation[this._operation.length - 1];
   }
 
-  setLastOperation(value){
-    this._operation[this._operation.length - 1] = value
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
   }
 
-  isOperator(value){
-  return  (['+', '-', '*', '%', '/'].indexOf(value)> -1)
+  isOperator(value) {
+    return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
   }
+
+  pushOperation(value){
+    this._operation.push(value);
+
+    if(this._operation.length>3){
+
+      let last = this._operation.pop();
+
+      this.calc();
+     
+    }
+  }
+
+  calc(){
+    let last = this._operation.pop();  
+
+   let result = eval(this._operation.join(""));
+
+   this._operation = [result, last];
+  }
+
+  setLastNumberToDisplay(){}
 
   addOperator(value) {
     if (isNaN(this.getLastOperation())) {
-      if(this.isOperator(value)){
+      if (this.isOperator(value)) {
         this.setLastOperation(value);
-      }else if(isNaN(value)){
-
-        console.log(value);
-      }else{
-        this._operation.push(value);
+      } else if (isNaN(value)) {
+        console.log("outracoisa",value);
+      } else {
+        this.pushOperation(value);
       }
-
-
     } else {
-      let newValue = this.getLastOperation().toString() + value.toString();
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
       this.setLastOperation(parseInt(newValue));
+
+        this.setLastNumberToDisplay();
+      }
+      
+      
     }
 
-
-    console.log(this._operation);
   }
 
   setError() {
@@ -77,25 +102,24 @@ class CalcController {
         this.clearEntry();
         break;
       case "soma":
-        this.addOperator('+');
+        this.addOperator("+");
         break;
       case "subtracao":
-        this.addOperator('-');
+        this.addOperator("-");
         break;
       case "divisao":
-        this.addOperator('/');
+        this.addOperator("/");
         break;
       case "multiplicacao":
-        this.addOperator('*');
+        this.addOperator("*");
         break;
       case "porcento":
-        this.addOperator('%');
+        this.addOperator("%");
         break;
       case "igual":
-      
         break;
       case "ponto":
-        this.addOperator('.');
+        this.addOperator(".");
         break;
       case "0":
       case "1":
